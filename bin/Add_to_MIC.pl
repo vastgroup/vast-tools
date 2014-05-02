700977afc6ccc559dc8ffc8a96a6760a3856431e
tim <tim.sterne.weiler@utoronto.ca>
2014-05-02 11:57:01 -0400

    added new qual output to MIC events;

--- bin/Add_to_MIC.pl
+++ bin/Add_to_MIC.pl
@@ -13,9 +13,10 @@ use Getopt::Long;
 my $dbDir;
 my $sp;
 my $samLen;
+my $legacyFlag;
 
 GetOptions("dbDir=s" =>\$dbDir, "sp=s" => \$sp, "len=i" => \$samLen,
-			  "verbose=i" => \$verboseFlag);
+			  "verbose=i" => \$verboseFlag, "legacy" => \$legacyFlag);
 
 sub verbPrint {
   my $verbMsg = shift;
@@ -46,27 +47,46 @@ foreach $file (@files){
 	$pre_data{$event}=join("\t",@t[0..5]) if !$pre_data{$event};
 
 	# Quality scores (only coverage)
-	$Q="";
-#### Score 1: Using all raw reads
-	if (($t[7]+$t[8])>=100){$Q="SOK";}
-	elsif ($t[7]>=20 || $t[8]>=20){$Q="OK";}
-	elsif ($t[7]>=15 || $t[8]>=15){$Q="LOW";}
-	elsif ($t[7]>=10 || $t[8]>=10){$Q="VLOW";}
-	else {$Q="N";}
-#### Score 2: Using all corrected reads
-	if (($t[9]+$t[10])>=100){$Q.=",SOK";}
-	elsif ($t[9]>=20 || $t[10]>=20){$Q.=",OK";}
-	elsif ($t[9]>=15 || $t[10]>=15){$Q.=",LOW";}
-	elsif ($t[9]>=10 || $t[10]>=10){$Q.=",VLOW";}
-	else {$Q.=",N";}
-#### Score 3: Using simple (=reference, C1A, AC2, C1C2) raw reads
-	if (($t[7]+$t[8])>=100){$Q.=",SOK";}
-	elsif ($t[7]>=20 || $t[8]>=20){$Q.=",OK";}
-	elsif ($t[7]>=15 || $t[8]>=15){$Q.=",LOW";}
-	elsif ($t[7]>=10 || $t[8]>=10){$Q.=",VLOW";}
-	else {$Q.=",N";}
+	$Q="";i
+   my $raw_reads_exc=$t[7];
+   my $raw_reads_inc=$t[8];
+   my $corr_reads_exc=$t[9];
+   my $corr_reads_inc=$t[10];
+#### Score 1: Using all raw reads                                                                                                       
+   if (($raw_reads_exc+$raw_reads_inc)>=100){$Q="SOK";}
+   elsif ($raw_reads_exc>=20 || $raw_reads_inc >=20){$Q="OK";}
+   elsif ($raw_reads_exc>=15 || $raw_reads_inc >=15){$Q="LOW";}
+   elsif ($raw_reads_exc>=10 || $raw_reads_inc >=10){$Q="VLOW";}
+   else {$Q="N";}
+#### Score 2: Using all corrected reads                                                                                                 
+   if (($corr_reads_exc+$corr_reads_inc)>=100){$Q.=",SOK";}
+   elsif ($corr_reads_exc>=20 || $corr_reads_inc>=20){$Q.=",OK";}
+   elsif ($corr_reads_exc>=15 || $corr_reads_inc>=15){$Q.=",LOW";}
+   elsif ($corr_reads_exc>=10 || $corr_reads_inc>=10){$Q.=",VLOW";}
+   else {$Q.=",N";}
+#### Score 3: Using simple (=reference, C1A, AC2, C1C2) raw reads                                                                       
+   if (($raw_reads_exc+$raw_reads_inc)>=100){$Q.=",SOK";}
+   elsif ($raw_reads_exc>=20 || $raw_reads_inc>=20){$Q.=",OK";}
+   elsif ($raw_reads_exc>=15 || $raw_reads_inc>=15){$Q.=",LOW";}
+   elsif ($raw_reads_exc>=10 || $raw_reads_inc>=10){$Q.=",VLOW";}
+   else {$Q.=",N";}
 #### No scores 4 and 5 for microexon pipeline.	
 	$Q.=",na,na";
+
+   ### DIFF OUTPUT ADDITION TO QUAL SCORE!
+   ### Essentially adding the expected number of reads re-distributed to INC or EXC after normalization..
+   ### These values are added to the qual score and used to infer the posterior distribution
+   unless($legacyFlag) {
+     my $totalN = $raw_reads_inc + $raw_reads_exc;
+     my($pPSI, $exValOfInc, $exValOfExc) = (0, 0, 0);
+     unless($PSI eq "NA" or $totalN < 2) {
+       $pPSI = $PSI / 100;
+       $exValOfInc = $pPSI * $totalN;
+       $exValOfExc = (1-$pPSI) * $totalN;
+     }
+     # ALTER QUAL OUTPUT HERE>>
+     $Q .= "\@$exValOfInc,$exValOfExc";
+   }
 
 	$PSI{$event}.="\t$t[6]\t$Q";
 	$read_counts{$event}.="\t".join("\t",@t[7..10])."\t$t[6]=$Q";
