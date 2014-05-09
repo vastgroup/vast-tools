61fb429210301f4aa1df1cf17e4da9fb30724ee3
Kevin Ha <kha@mail.utoronto.ca>
2014-05-09 12:16:33 -0400

    Revert "fixed merge3 to take STDIN now"
    This reverts commit 4863034b7d19f76a29010f3adfaa899f592ff64f.

--- bin/Add_to_MERGE3m.pl
+++ bin/Add_to_MERGE3m.pl
@@ -28,28 +28,67 @@ sub verbPrint {
 }
 
 
-verbPrint "Loading data for merge..\n";
-my $head1 = <STDIN>;
-chomp($head1);
-my(@header);
-push(@header, $head1);
+$EXSK=$ARGV[0]; # Cassette
+$MULTI=$ARGV[1]; # MULTI
+$COMBI=$ARGV[2]; # COMBI
+$MIC=$ARGV[3]; # MIC
 
-my(@head1) = split(/\t/,$head1);
-$head1 = join("\t",@head1[6..$#head1]);
+# checks if files exist and opens them
+open (I1, $EXSK) || die "Can't find the EXSK file\n";
+open (I2, $MULTI) || die "Can't find the MULTI file\n";
+open (I3, $COMBI) || die "Can't find the exsk5 file\n";
+open (I4, $MIC) || die "Can't find the micX file\n";
 
-while (my $l = <STDIN>){
-    chomp($l);
-    if($l =~ /^GENE/) { push(@header, $l); next; }
+verbPrint "Loading EXSK data\n";
+$head1=<I1>;
+@head1=split(/\t/,$head1);
+$head1=join("\t",@head1[6..$#head1]);
+while (<I1>){
+    chomp;
     @t=split(/\t/);
     $data{$t[1]}=join("\t",@t[6..$#t]);
 }
+close I1;
+
+verbPrint "Loading MULTI data\n";
+$head2=<I2>;
+@head2=split(/\t/,$head2);
+$head2=join("\t",@head2[6..$#head2]);
+while (<I2>){
+    chomp;
+    @t=split(/\t/);
+    $data{$t[1]}=join("\t",@t[6..$#t]);
+}
+close I2;
+
+verbPrint "Loading COMBI data\n";
+$head3=<I3>;
+@head3=split(/\t/,$head3);
+$head3=join("\t",@head3[6..$#head3]);
+while (<I3>){
+    chomp;
+    @t=split(/\t/);
+    $data{$t[1]}=join("\t",@t[6..$#t]);
+}
+close I3;
+
+verbPrint "Loading MIC data\n";
+$head4=<I4>;
+@head4=split(/\t/,$head4);
+$head4=join("\t",@head4[6..$#head4]);
+while (<I4>){
+    chomp;
+    @t=split(/\t/);
+    $data{$t[1]}=join("\t",@t[6..$#t]);
+}
+close I4;
 
 # Checks that all four files have the same samples in the same order
-die "[vast combine merge error]: Some of the headings are not identical!\n" if ($header[0] ne $header[1] || $header[0] ne $header[2] || $header[0] ne $header[3]);
+die "[vast combine merge error]: Some of the headings are not identical!\n" if ($head1 ne $head2 || $head1 ne $head3 || $head1 ne $head4);
 
 ### Output file
 ($sp,$N_samples)=$EXSK=~/\-(.{3})(\d+)\-n/;
-$output="raw_incl/INCLUSION_LEVELS_MERGE3m-$sp$N_samples-n.tab";
+$output="INCLUSION_LEVELS_MERGE3m-$sp$N_samples-n.tab";
 open (OUTPUT, ">$output");
 
 # Loads the template 
@@ -58,7 +97,7 @@ $head=<TEMPLATE>;
 chomp($head);
 @t=split(/\t/,$head);
 $pre_data=join("\t",@t[0..5]);
-print OUTPUT "$pre_data\t$head1\n";
+print OUTPUT "$pre_data\t$head1";
 while (<TEMPLATE>){
     chomp;
     @t=split(/\t/);
