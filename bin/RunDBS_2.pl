16717f8843128f1fce525fece6a783d16c9b57b7
Kevin Ha <kha@mail.utoronto.ca>
2014-05-09 11:43:49 -0400

    Chain commands and pipe to Add_to_MERGE3m.pl

--- bin/RunDBS_2.pl
+++ bin/RunDBS_2.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl -w
 # This pipeline takes PSI templates and adds PSIs from new samples.
+use strict;
 use Cwd qw(abs_path);
 use Getopt::Long;
 
@@ -75,28 +76,37 @@ mkdir("raw_reads") unless (-e "raw_reads"); # ^
 #$sp=$ARGV[0];
 die "Needs species 3-letter key\n" if !defined($sp);  #ok for now, needs to be better. --TSW
 
-@files=glob("spli_out/*exskX"); #gathers all exskX files (a priori, simple).
-$N=$#files+1;
+my @files=glob("spli_out/*exskX"); #gathers all exskX files (a priori, simple).
+my $N=$#files+1;
 
 ### Gets the PSIs for the events in the a posteriori pipeline
 verbPrint "Building Table for COMBI (a posteriori pipeline)\n";
-sysErrMsg "$binPath/Add_to_COMBI.pl -sp=$sp -dbDir=$dbDir -len=$globalLen -verbose=$verboseFlag";
+my $combi = "$binPath/Add_to_COMBI.pl -sp=$sp -dbDir=$dbDir -len=$globalLen -verbose=$verboseFlag";
 
 ### Gets the PSIs for the a priori, SIMPLE
 verbPrint "Building Table for EXSK (a priori pipeline, single)\n";
-sysErrMsg "$binPath/Add_to_APR.pl -sp=$sp -type=exskX -dbDir=$dbDir -len=$globalLen -verbose=$verboseFlag";
+my $exsk = "$binPath/Add_to_APR.pl -sp=$sp -type=exskX -dbDir=$dbDir -len=$globalLen -verbose=$verboseFlag";
 
 ### Gets the PSIs for the a priori, COMPLEX
 verbPrint "Building Table for MULTI (a priori pipeline, multiexon)\n";
-sysErrMsg "$binPath/Add_to_APR.pl -sp=$sp -type=MULTI3X -dbDir=$dbDir -len=$globalLen -verbose=$verboseFlag";
+my $multi = "$binPath/Add_to_APR.pl -sp=$sp -type=MULTI3X -dbDir=$dbDir -len=$globalLen -verbose=$verboseFlag";
 
 ### Gets the PSIs for the MIC pipeline
 verbPrint "Building Table for MIC (microexons)\n";
-sysErrMsg "$binPath/Add_to_MIC.pl -sp=$sp -dbDir=$dbDir -len=$globalLen -verbose=$verboseFlag";
+my $mic = "$binPath/Add_to_MIC.pl -sp=$sp -dbDir=$dbDir -len=$globalLen -verbose=$verboseFlag";
+
+### Chain the following commands together:
+###     Get PSIs for the events in the a posteriori pipeline
+###     Gets the PSIs for the a priori, SIMPLE
+###     Gets the PSIs for the a priori, COMPLEX
+###     Gets the PSIs for the MIC pipeline
+my $cmd =  "\($combi\; $exsk\; $multi\; $mic\)";
 
 ### Adds those PSIs to the full database of PSIs (MERGE3m).
 verbPrint "Building non-redundant PSI table (MERGE3m)\n";  # FIXED?? --TSW
-sysErrMsg "$binPath/Add_to_MERGE3m.pl raw_incl/INCLUSION_LEVELS_EXSK-$sp$N-n.tab raw_incl/INCLUSION_LEVELS_MULTI-$sp$N-n.tab raw_incl/INCLUSION_LEVELS_COMBI-$sp$N-n.tab raw_incl/INCLUSION_LEVELS_MIC-$sp$N-n.tab -sp=$sp -dbDir=$dbDir -len=$globalLen -verbose=$verboseFlag";
+my $merge = "$binPath/Add_to_MERGE3m.pl -sp=$sp -dbDir=$dbDir -len=$globalLen -verbose=$verboseFlag";
+
+sysErrMsg "$cmd | $merge";
 
 ### Gets PSIs for ALT5ss and adds them to the general database
 verbPrint "Building Table for Alternative 5'ss choice events\n";
@@ -105,4 +115,3 @@ sysErrMsg "$binPath/Add_to_ALT5.pl -sp=$sp -dbDir=$dbDir -len=$globalLen -verbos
 ### Gets PSIs for ALT3ss and adds them to the general database
 verbPrint "Building Table for Alternative 3'ss choice events\n";
 sysErrMsg "$binPath/Add_to_ALT3.pl -sp=$sp -dbDir=$dbDir -len=$globalLen -verbose=$verboseFlag";
-
