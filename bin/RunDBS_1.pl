c6b066584152351ae803957e08896dad6ef5a830
Kevin Ha <kha@mail.utoronto.ca>
2014-05-15 18:32:05 -0400

    Minor fixes to trim commands

--- bin/RunDBS_1.pl
+++ bin/RunDBS_1.pl
@@ -241,22 +241,22 @@ if (!$genome_sub){
 # TODO: substitute all of this with gawk 'NR%2==0{print substr($1,5,55)}NR%2==1' INPUT.fq... style 
  my $trimmed = 0;    # flag determining whether trimming occurred
  if ($difLE >= 10){
+   $cmd = getPrefixCmd($fq);
    if (!defined($trim) or $trim eq "twice"){
-      $cmd = getPrefixCmd($fq);
 	  if ($length > ($le*2)+10){
 	     $half_length = sprintf("%.0f", $length / 2);
          verbPrint "Trimming and splitting fastq sequences to $le nt sequences";
          sysErrMsg "$cmd | $binPath/Trim.pl --trim \"twice\" - $half_length | " .
-                    "$binPath/Trim-twiceOVER.pl --trim \"twice\" - $le | " .
+                    "$binPath/Trim-twiceOVER.pl --trim twice - $le | " .
                     "gzip > $root-$le.fq.gz";
 	  } else {
 	     verbPrint "Trimming and splitting fastq sequences to $le nt sequences";
-	     sysErrMsg "$cmd | $binPath/Trim.pl --trim \"twice\" - $le | " .
+	     sysErrMsg "$cmd | $binPath/Trim.pl --trim twice - $le | " .
                     "gzip > $root-$le.fq.gz";
 	  }
    } elsif ($trim eq "once"){
 	 verbPrint "Trimming fastq sequences to $le nt sequences";
-	 sysErrMsg "$binPath/Trim.pl --trim \"once\" $fq $le | gzip > $root-$le.fq";
+	 sysErrMsg "$cmd | $binPath/Trim.pl --trim once - $le | gzip > $root-$le.fq.gz";
    }
    $fq = "$root-$le.fq.gz"; # set new $fq with trimmed reads --KH
    $trimmed = 1;
