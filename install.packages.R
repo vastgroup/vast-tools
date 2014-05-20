b36dddce157afe787477de863445db96ff39921b
tim <tim.sterne.weiler@utoronto.ca>
2014-05-20 13:10:07 -0400

    inst work

--- install.packages.R
+++ install.packages.R
@@ -29,4 +29,10 @@ source(paste(c(scriptPath,"/R/Rlib/include.R"), collapse=""))
 # custom install from include.R
 loadPackages(c("optparse", "RColorBrewer", "reshape2", "ggplot2", "grid", "parallel"))
 
+if(system("which bowtie") > 0) {
+  stop("Cannot find 'bowtie' in path!!!  Please install this properly (e.x. /usr/bin/ ) or supply -bowtieProg flag to vast-tools!");
+} else {
+  writeLines("Found bowtie...");
+}
+
 q(status=0)
