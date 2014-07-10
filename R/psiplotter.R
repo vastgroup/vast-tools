a4246f347d951de1a8c7f6f8f86049ff8ece79d3
Kevin Ha <k.ha@mail.utoronto.ca>
2014-07-10 12:48:23 -0400

    Change plot title format

--- R/psiplotter.R
+++ R/psiplotter.R
@@ -161,7 +161,10 @@ get_beta_ci <- function(q) {
 
 format_table <- function(m) {
   # Format table to keep only PSIs and convert exon metadata as rownames
-  id <- paste(m$COMPLEX, m$GENE, m$COORD, m$LENGTH, sep="=")
+  id <- paste(m$GENE, m$COORD, 
+            paste(m$LENGTH, "nt"), 
+            paste("Type = ", m$COMPLEX), 
+            sep=", ")
   
   # Extract PSIs
   r <- convert_psi(m[,7:ncol(m)])
