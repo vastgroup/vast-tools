6d75f7c45e97e61ebbdbae15594a80950782ff16
Andre Gohr <andre.gohr@crg.eu>
2018-01-15 16:01:23 +0100

    changes: if info file not available, data is assumes to be not strand-sp

--- bin/Add_to_ALT5.pl
+++ bin/Add_to_ALT5.pl
@@ -92,11 +92,17 @@ foreach $file (@EEJ){
     $head_PSIs.="\t$sample\t$sample-Q";
     $head_ReadCounts.="\t$sample-Ri\t$sample-Rtot\t$sample-Q";
     
-    unless(-e "to_combine/${sample}.info"){ die "Do not find to_combine/${sample}.info. You might need to run vast-tools align again.";}
-    open(my $fh_info,"to_combine/${sample}.info") or die "$!"; my $line=<$fh_info>; close($fh_info);
-    my @fs=split("\t",$line);
-    if($fs[@fs-2] eq "-SS"){$is_ss{$sample}=1}
-    
+    unless(-e "to_combine/${sample}.info"){ verbPrint "$sample: do not find to_combine/${sample}.info. Sample will be treated as being not strand-specific.";
+    }else{
+    	open(my $fh_info,"to_combine/${sample}.info") or die "$!"; my $line=<$fh_info>; close($fh_info);
+    	my @fs=split("\t",$line);
+    	if($fs[@fs-2] eq "-SS"){
+    		$is_ss{$sample}=1;
+    		verbPrint "$sample: found to_combine/${sample}.info. Sample will be treated as being strand-specific."
+    	}else{
+    		verbPrint "$sample: found to_combine/${sample}.info. Sample will be treated as being not strand-specific."
+    	}
+    }   
 
     # Loads EEJ files
     open (EEJ, $file);
