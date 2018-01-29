6098a71c4cafd0f1c3912b77eaa5fd6ab0cea2bc
mirimia <mirimia@gmail.com>
2018-01-29 20:24:32 +0100

    Format print embelishment

--- bin/Add_to_ALT5.pl
+++ bin/Add_to_ALT5.pl
@@ -92,15 +92,15 @@ foreach $file (@EEJ){
     $head_PSIs.="\t$sample\t$sample-Q";
     $head_ReadCounts.="\t$sample-Ri\t$sample-Rtot\t$sample-Q";
     
-    unless(-e "to_combine/${sample}.info"){ verbPrint "$sample: do not find to_combine/${sample}.info. Sample will be treated as being not strand-specific.";
+    unless(-e "to_combine/${sample}.info"){ verbPrint "   $sample: do not find to_combine/${sample}.info. Sample will be treated as being not strand-specific.";
     }else{
     	open(my $fh_info,"to_combine/${sample}.info") or die "$!"; my $line=<$fh_info>; close($fh_info);
     	my @fs=split("\t",$line);
     	if($fs[@fs-2] eq "-SS"){
     		$is_ss{$sample}=1;
-    		verbPrint "$sample: found to_combine/${sample}.info. Sample will be treated as being strand-specific."
+    		verbPrint "   $sample: found to_combine/${sample}.info. Sample will be treated as being strand-specific."
     	}else{
-    		verbPrint "$sample: found to_combine/${sample}.info. Sample will be treated as being not strand-specific."
+    		verbPrint "   $sample: found to_combine/${sample}.info. Sample will be treated as being not strand-specific."
     	}
     }   
 
