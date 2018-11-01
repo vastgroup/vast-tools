d0fff2c95678a62a1ac40e5ef9ace5e66fe9803b
Toni Hermoso Pulido <toniher@cau.cat>
2018-11-01 18:35:28 +0100

    update test

--- tests/tests.sh
+++ tests/tests.sh
@@ -6,6 +6,7 @@ DIRBASE=$1
 DIROUT=$2
 
 # Compare with expression outcome file
-cmp --silent $DIRBASE/Test-100.cRPKM $DIROUT/expr_out/Test-100.cRPKM || exit 1 
+cmp --silent $DIRBASE/NewFQ_Test_Hsa-51/expr_out/NewFQ_Test_Hsa-51.cRPKM $DIROUT/expr_out/NewFQ_Test_Hsa-51.cRPKM || exit 1
+cmp --silent $DIRBASE/NewFQ_Test_Hsa-51/to_combine/NewFQ_Test_Hsa-51.MULTI3X $DIROUT/to_combine/NewFQ_Test_Hsa-51.MULTI3X || exit 1
 
 exit 0
